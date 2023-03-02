3fedcf300c3835486d51ae23cacd2b9adb31ca1a
Elijah Ribeiro <126627339+elijahribeiro@users.noreply.github.com>
2023-03-02 20:36:04 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,4 @@
 function toque(){
 
-	document.getElementById('menu_mobile')
-	.style.display='flex'
-
-	document.getElementById('btn_on')
-	.style.display='none'
-
-	document.getElementById('btn_off')
-	.style.display='inline'
-}
-
-function toqueoff(){
-	
-    document.getElementById('btn_off')
-    .style.display='none'
-
-	document.getElementById('menu_mobile')
-	.style.display='none'
-
-	document.getElementById('btn_on')
-	.style.display='inline'
+	alert("olá")
 }
